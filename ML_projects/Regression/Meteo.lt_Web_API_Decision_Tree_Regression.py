d3ca9d8dfa76aa3827b787dbd1ee1e018216ab09
Jonas T <tupciauskas.j@gmail.com>
2024-05-24 17:15:40 +0300

    Directory with Machine Learning projects.

---
+++ ML_projects/Regression/Meteo.lt_Web_API_Decision_Tree_Regression.py
@@ -0,0 +1,67 @@
+import requests
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+
+# json Request from API:
+HOST = 'https://api.meteo.lt/v1'
+r = requests.get(HOST + '/places/vilnius/forecasts/long-term')
+json = r.json()
+
+# Converting json to dataframe:
+dataset = pd.DataFrame.from_dict(json['forecastTimestamps'])
+
+# Saving json as .csv file:
+time_stamp = 'Forecast_at_' + datetime.now().strftime("%Y_%m_%d_%H_%M") + '.csv'
+dataset.to_csv(time_stamp, index=False)
+print(time_stamp)
+
+# Splitting dataframe to X and y dataframes:
+X = dataset.iloc[:, 3:].values
+y = dataset.iloc[:, 1].values
+
+# Encoding categorical data:
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [-1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+# Checking for NaN values, if there are any, replace them with mean values:
+if dataset.isnull().values.any():
+    # Filling missing dataframe values:
+    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imputer.fit(X)
+    X = imputer.transform(X)
+
+# Splitting the dataset into the Training set and Test set:
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+# Training the Decision Tree Regression model on the Training set:
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results:
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# Evaluating the Model Performance:
+print(r2_score(y_test, y_pred))
+
+# Visualising the Decision Tree Regression results against dataframe values:
+plt.scatter(dataset.index[0:len(y_test)], y_test, color='red')
+plt.scatter(dataset.index[0:len(y_test)], y_pred, color='blue')
+plt.plot(dataset.index[0:len(y_test)], y_test, color='red', label='Test values')
+plt.plot(dataset.index[0:len(y_test)], y_pred, color='blue', label='Predicted values')
+plt.title(f'Decision Tree Regression\nr^2={round(r2_score(y_test, y_pred), 3)}')
+plt.xlabel('Index')
+plt.ylabel('Temperature (C)')
+plt.grid(True)
+plt.legend()
+plt.show()
---
+++ ML_projects/Regression/Meteo.lt_Web_API_Random_Forest_Regression.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Importing the libraries
+
+# In[19]:
+
+
+import requests
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# ## json Request from API
+
+# In[20]:
+
+
+HOST = 'https://api.meteo.lt/v1'
+r = requests.get(HOST+'/places/vilnius/forecasts/long-term')
+json = r.json()
+
+
+# In[21]:
+
+
+time_stamp = 'Forecast_at_' + datetime.now().strftime("%Y_%m_%d_%H_%M") + '.csv'
+print(time_stamp)
+
+
+# ## Converting json to dataframe
+
+# In[22]:
+
+
+dataset = pd.DataFrame.from_dict(json['forecastTimestamps'])
+
+
+# ## Checking for NaN values
+
+# In[23]:
+
+
+dataset.isnull().values.any()
+
+
+# In[24]:
+
+
+dataset.isnull().sum()
+
+
+# ## Saving dataframe to .csv file
+
+# In[25]:
+
+
+dataset.to_csv(time_stamp, index=False)
+
+
+# ## Plotting dataframe
+
+# In[26]:
+
+
+plt.plot(dataset['forecastTimeUtc'].index[:], dataset.iloc[:,1].values, color='black')
+plt.scatter(dataset['forecastTimeUtc'].index[:], dataset.iloc[:,1].values, color='black')
+plt.xlabel(dataset.iloc[0,0][0:-3] + " " + dataset.iloc[-1,0][0:-3])
+plt.ylabel('Temperature (C)')
+plt.show()
+
+
+# ## Filtering dataframe by hour
+
+# In[27]:
+
+
+# data_by_hour = dataset[dataset['forecastTimeUtc'].str.contains('00:00:00')]
+
+
+# In[28]:
+
+
+# plt.plot(data_by_hour['forecastTimeUtc'].index[:56], data_by_hour['airTemperature'], color='red')
+# plt.xlabel(dataset.iloc[0,0][0:11] + dataset.iloc[-1,0][0:10])
+# plt.ylabel('Temperature (C)')
+# plt.show()
+
+
+# ## Splitting dataframe to X and y dataframes
+
+# In[29]:
+
+
+X = dataset.iloc[:, 3:].values
+y = dataset.iloc[:,1].values
+
+
+# ## Encoding categorical data
+
+# In[30]:
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [-1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+
+# ## Filling missing dataframe values
+
+# In[31]:
+
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer.fit(X)
+X = imputer.transform(X)
+
+
+# ## Splitting the dataset into the Training set and Test set
+
+# In[32]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+
+
+# ## Training the Random Forest Regression model on the Training set
+
+# In[37]:
+
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X_train, y_train)
+
+
+# ## Visualising the Random Forest Regression results with dataframe results
+
+# In[38]:
+
+
+plt.scatter(dataset.index[0:len(y_test)], y_test, color = 'red')
+plt.scatter(dataset.index[0:len(y_test)], regressor.predict(X_test), color = 'blue')
+plt.plot(dataset.index[0:len(y_test)], y_test, color = 'red')
+plt.plot(dataset.index[0:len(y_test)], regressor.predict(X_test), color = 'blue')
+plt.title('Random Forest Regression')
+plt.xlabel('Index')
+plt.ylabel('Temperature (C)')
+plt.show()
+
+
+# ## Predicting the Test set results
+
+# In[39]:
+
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+
+# ## Evaluating the Model Performance
+
+# In[40]:
+
+
+from sklearn.metrics import r2_score
+r2_score(y_test, y_pred)
+
+
+# In[ ]:
+
+
+
+
